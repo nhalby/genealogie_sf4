9f3806c59166090a8fb92e486747b672599a40a8
nhalby <nhalby@gmail.com>
2020-06-03 19:00:56 +0200

    Mise en place de phpcodesnifer + php-cs-fixer

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -1,9 +1,19 @@
 <?php
 
+/**
+ * Ceci sera ajouté dans tous vos fichiers PHP en entête.
+ *
+ * (c) Zozor <zozor@openclassrooms.com>
+ *
+ * A adapter et ré-utiliser selon vos besoins!
+ */
+
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
@@ -12,17 +22,40 @@ class SecurityController extends AbstractController
      */
     public function index()
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/SecurityController.php',
-        ]);
+        return $this->json(
+            ['message' => 'Welcome to your new controller!',
+            'path' => 'src/Controller/SecurityController.php',]
+        );
     }
 
     /**
-     * @Route("/login", name="user_login")
+     * @Route("/login", name="app_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('login.html.twig', []);
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig', 
+            ['last_username' => $lastUsername, 'error' => $error]
+        );
+    }
+
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \LogicException(
+            'This method can be blank - 
+            it will be intercepted by the logout key on your firewall.'
+        );
     }
 }
